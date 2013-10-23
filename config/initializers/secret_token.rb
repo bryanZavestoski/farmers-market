dde9c8f9ddb4978e62b12ae063cb023a8d43b9ab
Bryan Zavestoski <bzavestoski@gmail.com>
2013-10-22 17:08:42 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FarmersMarket::Application.config.secret_key_base = 'b0a96578ebf19b3e54c623fbf1aee0ad9f0dd5b638cfed39f2857e16f8121448eca78fa49d64c0e73439da8cb9e6e6606bc6f50a9f234399f6b05616883bcbab'
